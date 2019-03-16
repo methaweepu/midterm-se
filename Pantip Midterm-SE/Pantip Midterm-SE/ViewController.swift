98b8463e55f76dda97650dfa894cafb087454603
methawee <methawee.pu@ku.th>
2019-03-16 14:39:24 +0700

    Create Pantip project

---
+++ Pantip Midterm-SE/Pantip Midterm-SE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pantip Midterm-SE
+//
+//  Created by Methawee Punkaew on 16/3/19.
+//  Copyright © 2019 Methawee Punkaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
