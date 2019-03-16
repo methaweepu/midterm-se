458628cace8474665cf4254a951dd4ecb3464525
methawee <methawee.pu@ku.th>
2019-03-16 14:58:59 +0700

    #login ui page

---
+++ Pantip Midterm-SE/Pantip Midterm-SE/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Pantip Midterm-SE
+//
+//  Created by Methawee Punkaew on 16/3/19.
+//  Copyright © 2019 Methawee Punkaew. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
